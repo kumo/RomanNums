c9d24e3136ecef8d75e57dc17103f405dacad43d
kumo <kumo@kumo.it>
2008-08-13 16:46:53 +0200

    converting arabic to roman (not useful)

--- Classes/RomanViewController.m
+++ Classes/RomanViewController.m
@@ -25,12 +25,45 @@
 - (IBAction)convertYear:(id)sender {
 	
     self.string = textField.text;
-	
     NSString *nameString = string;
     if ([nameString length] == 0) {
         nameString = @"2008";
     }
-    NSString *greeting = [[NSString alloc] initWithFormat:@"The year is %@!", nameString];
+	int length = [nameString length];
+	
+    int i, deflate = 2008;
+    NSString *romanValue = @"";
+    NSDictionary *pairs = [NSDictionary dictionaryWithObjectsAndKeys:
+						   @"i", @"1",
+						   @"x", @"10",
+						   @"c", @"100",
+						   @"m", @"1000",
+						   @"iv", @"4",
+						   @"xl", @"40",
+						   @"cd", @"400",
+						   @"v", @"5",
+						   @"l", @"50",
+						   @"d", @"500",
+						   @"ix", @"9",
+						   @"xc", @"90",
+						   @"cm", @"900",
+						   nil];
+	
+    NSArray *values = [NSArray arrayWithObjects:@"1000", @"900", @"500", @"400", @"100", @"90", @"50", @"40", @"10", @"9", @"5", @"4", @"1", nil];
+    int itemCount = [values count], itemValue;
+    
+    for (i = 0; i < itemCount; i++)
+    {
+        itemValue = [[values objectAtIndex:i] intValue];
+        
+        while (deflate >= itemValue)
+        {
+            romanValue = [romanValue stringByAppendingString:[pairs objectForKey:[values objectAtIndex:i]]];
+            deflate -= itemValue;
+        }
+    }
+    
+    NSString *greeting = [[NSString alloc] initWithFormat:@"The year is %@ [%d chars]! (%@)", nameString, length, romanValue];
     label.text = greeting;
     [greeting release];
 }
