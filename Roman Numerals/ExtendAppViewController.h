474b40958a282abc312f81334c9a291c2f08780c
Rob Clarke <kumo@kumo.it>
2014-02-16 22:54:31 +0100

    Moving in-app purchasing stuff to another view

---
+++ Roman Numerals/ExtendAppViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExtendAppViewController.h
+//  Roman Numerals
+//
+//  Created by Robert Clarke on 16/02/2014.
+//  Copyright (c) 2014 Robert Clarke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExtendAppViewController : UITableViewController
+
+@end
