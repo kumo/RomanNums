784c62b3d1bdd02f15c4f61f4dcef0e3ef88ff61
Rob Clarke <kumo@kumo.it>
2014-06-25 11:21:09 +0200

    Repositioning "extra purchases"

--- Roman Numerals/MainMenuViewController.m
+++ Roman Numerals/MainMenuViewController.m
@@ -67,11 +67,11 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     if (section == 0) {
-        return 1;
-    } else if (section == 1) {
         return 2;
-    } else {
+    } else if (section == 1) {
         return 3;
+    } else {
+        return 1;
     }
 }
 
@@ -79,16 +79,16 @@
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
     
-    if (indexPath.section == 1) {
+    if (indexPath.section == 0) {
         if (indexPath.row == 0) {
             [cell.textLabel setText:@"Tutorial"];
         } else if (indexPath.row == 1) {
             [cell.textLabel setText:@"Support"];
         }
-    } else if (indexPath.section == 0) {
+    } else if (indexPath.section == 2) {
         // restore purchases
         [cell.textLabel setText:@"Extra Features"];
-    } else if (indexPath.section == 2) {
+    } else if (indexPath.section == 1) {
         if (indexPath.row == 0) {
             [cell.textLabel setText:@"What's New"];
         } else if (indexPath.row == 1) {
@@ -107,19 +107,19 @@
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 
-    if (indexPath.section == 0) {
+    if (indexPath.section == 2) {
         if (indexPath.row == 0)  {
             ExtendAppViewController *myController = (ExtendAppViewController *)[self.storyboard instantiateViewControllerWithIdentifier:@"ExtendAppViewController"];
             [self presentViewController:myController animated:YES completion:NULL];
         }
-    } else if (indexPath.section == 1) {
+    } else if (indexPath.section == 0) {
         if (indexPath.row == 0)  {
             // tutorial
         } else if (indexPath.row == 1) {
             NSURL *url = [[NSURL alloc] initWithString:@"http://cadigatt.com/roman-nums/support/"];
             [[UIApplication sharedApplication] openURL:url];
         }
-    } else if (indexPath.section == 2) {
+    } else if (indexPath.section == 1) {
         if (indexPath.row == 0) {
             UIViewController *myController = (UIViewController *)[self.storyboard instantiateViewControllerWithIdentifier:@"WhatsNewViewController"];
             [self presentViewController:myController animated:YES completion:NULL];
